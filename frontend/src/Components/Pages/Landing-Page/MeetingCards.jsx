bcb6abbbf2d0be24197cf9a3af184cb702a25ad2
thecuriousteam <curiousdevelopers.in@gmail.com>
2024-05-11 22:03:48 +0530

    Revert "developed meeting cards"
    This reverts commit 46a2177b0a393bd6dd84b8694c4d108aeab78c3c.

--- frontend/src/Components/Pages/Landing-Page/MeetingCards.jsx
+++ frontend/src/Components/Pages/Landing-Page/MeetingCards.jsx
@@ -1,34 +1,9 @@
 import React from 'react'
-import IndividualCard from './IndividualCard'
 
 const MeetingCards = () => {
-  const data = [
-    {
-      logo:<i class="ri-add-line"></i>,
-      heading:"Video",
-      desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, non!"
-    },
-    {
-      logo:<i class="ri-user-add-fill"></i>,
-      heading:"Video",
-      desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, non!"
-    },
-    {
-      logo:<i class="ri-calendar-line"></i>,
-      heading:"Video",
-      desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, non!"
-    },
-    {
-      logo:<i class="ri-live-line"></i>,
-      heading:"Video",
-      desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, non!"
-    }
-  ]
   return (
-    <div className='w-[90%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 py-5'>
-      {data.map((ele, index) =>{
-        return <IndividualCard dataProps={ele} key={index}/>
-      })}
+    <div>
+      Meeting Cards Section
     </div>
   )
 }
