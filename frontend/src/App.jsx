be3e9e4a25b3f5a508ab54ec1d06f0a03dd594e0
sarthu210 <sarthu102@gmail.com>
2024-05-11 19:18:49 +0530

    the Footer Component i.e. the component which will be common for the whole App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { Footer } from '../components'
+
 const App = () => {
   return (
     <div>
       <h1 className='text-xl text-center '>Curious Connect</h1>
-      <Footer />
     </div>
     
   )
