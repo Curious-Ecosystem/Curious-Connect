3ae0a04cf13711cb72b85d2844d32679f4387e2b
Thirumuru Rishith Kumar Reddy <135592795+RishithReddy9@users.noreply.github.com>
2024-05-11 17:18:57 +0530

    Completed Footer component For Curious Connect

--- frontend/src/Components/common/Footer.jsx
+++ frontend/src/Components/common/Footer.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import Logo from '../../assets/Footer-Assets/Footer-logo.png'
+import Discord from '../../assets/Footer-Assets/Discord.png'
+import Facebook from '../../assets/Footer-Assets/Facebook F.png'
+import Youtube from '../../assets/Footer-Assets/YouTube.png'
+import Google from '../../assets/Footer-Assets/Google.png'
 
 const Footer = () => {
+  const year = new Date().getFullYear()
   return (
-    <div>
-      Footer Section
-    </div>
+    <div className='flex justify-between px-10 py-8' style={{ backgroundColor: '#000000' }}>
+      <div>
+        <img src={Logo} alt="Logo" />
+        <div className='pt-2'>
+          <p className='text-slate-400'>&copy; {year} CURIOUS CONNECT, ALL RIGHTS RESERVED.</p>
+        </div>
+      </div>
+      <div className='flex gap-3'>
+        <div className='cursor-pointer'>
+          <img src={Discord} alt="Discord" />
+        </div>
+        <div className='cursor-pointer'>
+          <img src={Facebook} alt="Facebook" />
+        </div>
+        <div className='cursor-pointer'>
+          <img src={Youtube} alt="Youtube" />
+        </div>
+        <div className='cursor-pointer'>
+          <img src={Google} alt="Google" />
+        </div>
+      </div>
+    </div >
   )
 }
 
