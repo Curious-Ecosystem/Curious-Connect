b614a23597093cac69be940793d285136193d66e
Nitesh Kumar Singh <138958926+niteshbca@users.noreply.github.com>
2024-05-11 22:30:34 +0530

    Add the hero section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,28 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className='text-xl text-center '>Curious Connect</h1>
+    <header>
+    <nav>
+      <ul>
+        <li><a href="app.jsx">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+      <h1 class="logo">mbe-dev</h1>
+    </nav>
+  </header>
+  <main>
+    <div class="container">
+      <img src="./res-img.png" alt=""/>
+      <div class="hero-text">
+        <h1>Your perfect coffee experience</h1>
+        <p>Indulge in artisanal coffee delights. Crafted with passion, brewed with perfection.
+          Savor the Flavor!
+        </p>
+        <button>Order Now</button>
+      </div>
+    </div>
+  </main>
     </div>
     
   )
