50f26649e04f178a3b1c227fe0eb742642110ab2
yashmandi <yashmandi18@gmail.com>
2024-05-13 16:44:59 +0530

    folder fix-2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { LandingPage } from "./Pages/Landing-Page";
-import AboutPage from "./Components/common/AboutSection/AboutPage";
-import Contact from "./Components/common/AboutSection/Contact";
-import FAQ from "./Components/common/AboutSection/FAQ";
+import AboutPage from "./Components/common/Footer/AboutSection/AboutPage";
+import Contact from "./Components/common/Footer/AboutSection/Contact";
+import FAQ from "./Components/common/Footer/AboutSection/FAQ";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Footer from "./Components/common/Footer";
+import Footer from "./Components/common/Footer/Footer";
 
 const App = () => {
   return (
