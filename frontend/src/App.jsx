ab5aac2bb2c2d74925c33d966ac4812562d17234
Aakarsh Verma <ansh62050@gmail.cim>
2024-05-14 15:12:45 +0530

    Added .prettierrc file for identations

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { LandingPage } from "./Pages/Landing-Page";
-import AboutPage from "./Components/common/Footer/AboutSection/AboutPage";
-import Contact from "./Components/common/Footer/AboutSection/Contact";
-import FAQ from "./Components/common/Footer/AboutSection/FAQ";
+import React from 'react';
+import { LandingPage } from './Pages/Landing-Page';
+import AboutPage from './Components/common/Footer/AboutSection/AboutPage';
+import Contact from './Components/common/Footer/AboutSection/Contact';
+import FAQ from './Components/common/Footer/AboutSection/FAQ';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Footer from "./Components/common/Footer/Footer";
-import PrivacyPolicyPage from "./Components/common/Footer/LegalSection/PrivacyPolicyPage";
-import TermsOfService from "./Components/common/Footer/LegalSection/TermsOfService";
+import Footer from './Components/common/Footer/Footer';
+import PrivacyPolicyPage from './Components/common/Footer/LegalSection/PrivacyPolicyPage';
+import TermsOfService from './Components/common/Footer/LegalSection/TermsOfService';
 
 const App = () => {
   return (
@@ -22,7 +22,6 @@ const App = () => {
           {/* legal section  */}
           <Route path="/PrivacyPolicyPage" element={<PrivacyPolicyPage />} />
           <Route path="/termsOfService" element={<TermsOfService />} />
-
         </Routes>
         <Footer />
       </Router>
--- frontend/src/Components/Pages/Landing-Page/HeroSection.jsx
+++ frontend/src/Components/Pages/Landing-Page/HeroSection.jsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react';
 
 // Design Hero section here and call this hero section in "frontend/src/Pages/Landing-Page.jsx"
 
 const HeroSection = () => {
-	return (
-		<div className="w-full h-full text-cyan-50 text-2xl text-center">
-			Hero Section
-		</div>
-	);
+  return (
+    <div className="w-full h-full text-cyan-50 text-2xl text-center">
+      Hero Section
+    </div>
+  );
 };
 
 export default HeroSection;
