ea0ad4d09634376de13c96a6d4e108cca328634f
Nitesh Kumar Singh <138958926+niteshbca@users.noreply.github.com>
2024-05-14 17:11:44 +0530

    Create a HeroSection responsive

--- frontend/src/Components/Pages/Landing-Page/LandingHeader.jsx
+++ frontend/src/Components/Pages/Landing-Page/LandingHeader.jsx
@@ -3,7 +3,27 @@ import React from "react";
 const Header = () => {
   return (
     <div className="w-full h-full text-cyan-50 text-2xl text-center">
-      Header Section
+  <main>
+    <div class="container">
+      <img src="meeting.png" alt=""/>
+      <div class="hero-text">
+        <h1>CONNECT</h1>
+        <h1> COLLABORATE </h1>
+        <h1>START CONFERENCE</h1>
+        <p>START YOUR NEXT VIDEO CALL WITH A SINGLE CLICK.
+          NO DOWNLOAD, PLUG-IN, OR LOGIN IS REQUIRED.
+          JUST GET STRAIGHT TO TALKING, MESSAGING, AND
+          SHARING YOUR SCREEN.
+        </p>
+        <button class="sign">SIGN IN</button>
+        <button class="sign">SIGN UP</button>
+      </div>
+
+    </div>
+  </main>
+<br/>  <div class="div"><button class="h1">PASTE THE MEETINO CODE</button>
+        <button class="h2"> 11111CURIOUSCONNECT</button><button>JOIN ROOM</button>
+        <button>🥨</button></div>
     </div>
   );
 };
