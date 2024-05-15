142ca7f482994d2f7d75f4e8aa7e4ad652c92b4d
ujjwalexcodie <kumarujjwalagrawal4@gmail.com>
2024-05-16 02:34:23 +0530

    Added Zod Validation to the Backend with Some basic Corrections in Autherization File
    Signed-off-by: ujjwalexcodie <kumarujjwalagrawal4@gmail.com>

--- services/Auth/src/routes/auth.route.js
+++ services/Auth/src/routes/auth.route.js
@@ -2,11 +2,12 @@ const express = require("express");
 
 const router = express.Router();
 const { signup, signin } = require("../controllers/auth.controller");
-
+const validate = require("../middleware/validate.schema");
+const RegisterSchema = require("../validation/zodschema");
 // auth routes...;
 
-router.post("/sign-up", signup);
+router.post("/sign-up",validate(RegisterSchema), signup);
 
-router.post("/sign-in", signin);
+router.post("/sign-in",validate(RegisterSchema), signin);
 
 module.exports = router;
---
+++ services/Auth/src/validation/zodschema.js
@@ -0,0 +1,25 @@
+const zod  = require("zod");
+
+
+const RegisterSchema = zod.object({
+    name: zod
+      .string({ required_error: "Email is required" })
+      .trim()
+      .email({ message: "Invalid Email Address" })
+      .min(3, { message: "Email should be at least 3 characters" })
+      .max(255, { message: "Email can be at most 255 characters" }),
+    password: zod
+      .string({ required_error: "Password is required" })
+      .trim()
+      .min(7, { message: "Password must be at least 7 characters" })
+      .max(1024, { message: "Password must be at most 1024 characters" }),
+    email: zod
+      .string({ required_error: "Email is required" })
+      .trim()
+      .email({message : "invalide Email Address"})
+      .min(3, { message: "Email must be at least of 3 characters" })
+      .max(255, { message: "Email must be more than 255 characters" }),
+  });
+
+  
+  module.exports = RegisterSchema ;
