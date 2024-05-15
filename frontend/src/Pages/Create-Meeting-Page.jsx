90b2c6f9d7b4c12b700061c095165cfb7753b226
Aakarsh Verma <ansh62050@gmail.cim>
2024-05-15 14:04:22 +0530

    Added .prettier for formatting

--- frontend/src/Pages/Create-Meeting-Page.jsx
+++ frontend/src/Pages/Create-Meeting-Page.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import EmptyContainer from '../Components/Pages/Meeting-Page/Create-Meeting'
+import React from 'react';
+import EmptyContainer from '../Components/Pages/Meeting-Page/Create-Meeting';
 
 const CreateMeeting = () => {
-    return (
-        <div>
-            <EmptyContainer />
-        </div>
-    )
-}
+  return (
+    <div>
+      <EmptyContainer />
+    </div>
+  );
+};
 
-export default CreateMeeting
+export default CreateMeeting;
