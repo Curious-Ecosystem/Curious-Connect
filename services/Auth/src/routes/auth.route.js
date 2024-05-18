293376d2cfe576178c393b9da125304b5668c05e
ujjwalexcodie <kumarujjwalagrawal4@gmail.com>
2024-05-18 12:14:59 +0530

    added another Zod Schema for the Sign-in Errors

--- services/Auth/src/routes/auth.route.js
+++ services/Auth/src/routes/auth.route.js
@@ -3,11 +3,10 @@ const express = require("express");
 const router = express.Router();
 const { signup, signin } = require("../controllers/auth.controller");
 const validate = require("../middleware/validate.schema");
-const RegisterSchema = require("../validation/zodschema");
+const { RegisterSchema, SigninSchema } = require("../validation/zodschema");
 // auth routes...;
 
 router.post("/sign-up",validate(RegisterSchema), signup);
-
-router.post("/sign-in",validate(RegisterSchema), signin);
+router.post("/sign-in",validate(SigninSchema), signin);
 
 module.exports = router;
