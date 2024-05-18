4c130df4ad791c2ef27a7877515c27b67957922c
sarthu210 <sarthu102@gmail.com>
2024-05-18 16:14:08 +0530

    Signin page updated with api request setup

--- frontend/src/Components/common/SignIn.jsx
+++ frontend/src/Components/common/SignIn.jsx
@@ -37,7 +37,7 @@ const SignIn = () => {
   
 
   return <div>
-    <div className="flex justify-center items-center px-12 py-12 max-md:px-2">
+    <div className="flex justify-center items-center px-12 py-8 max-md:px-2">
       <div className="flex lg:flex-nowrap flex-wrap flex-row gap-10 justify-between w-full max-w-[1209px] max-md:flex-wrap max-md:max-w-full">
         <div className="flex flex-col ml-1 self-start font-extrabold text-white max-md:mt-10">
         <div className="flex gap-4 mb-16 lg:mt-10 text-xl font-bold tracking-wide text-center text-white">
@@ -69,10 +69,18 @@ const SignIn = () => {
             PASSWORD
           </div>
           <input type='password' name='password' placeholder='Enter Your Password' className="shrink-0 mt-4 p-5 h-14 rounded-3xl bg-white" onChange={handlePasswordChange}></input>
-          <div className="justify-center cursor-pointer hover:bg-transparent hover:border-[2px] hover:border-[#F06B48] self-center px-16 py-3 mt-8 text-2xl font-bold text-center text-white whitespace-nowrap bg-[#F06B48] rounded-full max-md:px-14 max-md:mt-10" onClick={handleSubmit}>
+          <a href='#' className="self-start underline ml-2 mt-2 text-[13px] text-[#F06B48]">
+            Forgot Password?
+          </a>
+          <div className="justify-center cursor-pointer hover:bg-transparent hover:border-[2px] hover:border-[#F06B48] self-center px-16 py-3 mt-2 text-2xl font-bold text-center text-white whitespace-nowrap bg-[#F06B48] rounded-full max-md:px-14 max-md:mt-8 max-md:mb-5" onClick={handleSubmit}>
             Login
           </div>
-          <div className="flex flex-wrap gap-4 mt-12 justify-center max-md:mx-2.5 max-md:mt-10">
+          <div className='flex gap-2 text-center text-white mt-5'>
+            <div className='h-[1px] mt-2 w-[20%] items-center bg-white'></div>
+            <div className='justify-center w-[60%]'>You Can Also Login With</div>
+            <div className='h-[1px] mt-2 w-[20%] bg-white'></div>
+          </div>
+          <div className="flex flex-wrap gap-4 mt-5 justify-center max-md:mx-2.5 max-md:mt-10">
             <img src='https://cdn-icons-png.flaticon.com/512/2504/2504911.png' className="shrink lg:h-[70px] lg:w-[70px] h-[50px] w-[50px]" />
             <img src='https://cdn-icons-png.flaticon.com/512/2504/2504914.png' className="shrink lg:h-[70px] lg:w-[70px] h-[50px] w-[50px]" />
             <img src='https://cdn-icons-png.flaticon.com/512/2504/2504903.png' className="shrink lg:h-[70px] lg:w-[70px] h-[50px] w-[50px]" />
