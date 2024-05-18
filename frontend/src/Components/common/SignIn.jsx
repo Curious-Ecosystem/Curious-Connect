86f2da0e9bb76ddb4ea986a1337c86e6956a8753
sarthu210 <sarthu102@gmail.com>
2024-05-18 15:54:23 +0530

    Signin page updated with api request setup

--- frontend/src/Components/common/SignIn.jsx
+++ frontend/src/Components/common/SignIn.jsx
@@ -1,6 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const SignIn = () => {
+
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  }
+
+  async function handleSubmit() {
+    console.log(email, password);
+    const response = await fetch('LOGIN API', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email,
+        password
+      })
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+    } else {
+      console.log('Failed to login');
+    }
+  }
+
+  
+
   return <div>
     <div className="flex justify-center items-center px-12 py-12 max-md:px-2">
       <div className="flex lg:flex-nowrap flex-wrap flex-row gap-10 justify-between w-full max-w-[1209px] max-md:flex-wrap max-md:max-w-full">
@@ -29,12 +64,12 @@ const SignIn = () => {
           <div className="self-start mt-14 ml-2 text-lg font-bold text-white max-md:mt-10 max-md:ml-2.5">
             EMAIL
           </div>
-          <input type='text' name='email' placeholder='Enter Your Email' className="shrink-0 mt-4 p-5 h-14 rounded-3xl bg-white"></input>
+          <input type='text' name='email' placeholder='Enter Your Email' className="shrink-0 mt-4 p-5 h-14 rounded-3xl bg-white" onChange={handleEmailChange}></input>
           <div className="self-start mt-4 ml-2 text-lg font-bold text-white max-md:ml-2.5">
             PASSWORD
           </div>
-          <input type='password' name='password' placeholder='Enter Your Password' className="shrink-0 mt-4 p-5 h-14 rounded-3xl bg-white"></input>
-          <div className="justify-center cursor-pointer hover:bg-transparent hover:border-[2px] hover:border-[#F06B48] self-center px-16 py-3 mt-8 text-2xl font-bold text-center text-white whitespace-nowrap bg-[#F06B48] rounded-full max-md:px-14 max-md:mt-10">
+          <input type='password' name='password' placeholder='Enter Your Password' className="shrink-0 mt-4 p-5 h-14 rounded-3xl bg-white" onChange={handlePasswordChange}></input>
+          <div className="justify-center cursor-pointer hover:bg-transparent hover:border-[2px] hover:border-[#F06B48] self-center px-16 py-3 mt-8 text-2xl font-bold text-center text-white whitespace-nowrap bg-[#F06B48] rounded-full max-md:px-14 max-md:mt-10" onClick={handleSubmit}>
             Login
           </div>
           <div className="flex flex-wrap gap-4 mt-12 justify-center max-md:mx-2.5 max-md:mt-10">
