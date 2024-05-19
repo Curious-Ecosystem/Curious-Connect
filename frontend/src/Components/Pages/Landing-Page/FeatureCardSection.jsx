aeb6654675fd0b0e42e8e35bead634a454bf9407
Harshil Gupta <harshilgupta.2005@gmail.com>
2024-05-19 20:21:10 +0530

    FeatureCards Updated

--- frontend/src/Components/Pages/Landing-Page/FeatureCardSection.jsx
+++ frontend/src/Components/Pages/Landing-Page/FeatureCardSection.jsx
@@ -28,8 +28,8 @@ const featuresData = [
 ];
 const Feature = () => {
   return (
-    <div className='w-full h-full text-cyan-50 text-2xl text-center p-5 md:p-5 mt-5'>
-      <h5 className='w-full h-full text-cyan-50 text-2xl md:text-3xl leading-8 md:leading-3 tracking-normal p-3 font-semibold text-center'>
+    <div className='w-full h-full text-cyan-50 text-2xl text-center p-4 sm:p-5 md:p-5 mt-5'>
+      <h5 className='w-full h-full text-cyan-50 text-2xl md:text-3xl leading-8 md:leading-3 tracking-normal p-2 sm:p-3 font-semibold text-center'>
         Curious Connect Unique Attributes
       </h5>
       <p className='text-sm md:text-xl font-normal tracking-normal text-gray-500 p-3 w-[98%] md:w-[70%] mx-auto'>
@@ -37,20 +37,20 @@ const Feature = () => {
         quality lowest latency that makes your calls crystal clear. Compatible
         with all browsers and platforms!
       </p>
-      <div className='m-4 flex flex-row justify-between flex-wrap'>
+      <div className='m-0 sm:m-4 flex flex-row justify-between flex-wrap'>
         {featuresData.map((feature, index) => (
           <div
             key={index}
-            className='p-5 my-6 xl:my-0 xl:w-[22%] w-[100%] sm:w-[45%] transition-all duration-[0.5s] select-none ease-in-out delay-0 rounded-lg bg-gradient-to-r from-purple-500 to-pink-500 shadow hover:scale-105'>
+            className='p-2 sm:p-5 my-6 bg-white xl:my-0 xl:w-[22%] w-[48%] sm:w-[45%] transition-all duration-[0.5s] select-none ease-in-out delay-0 rounded-lg shadow hover:scale-105'>
             <img
-              className='w-20 h-20 p-3'
+              className='sm:w-20 sm:h-20 sm:p-3 w-12 h-12 p-2'
               src={feature.icon}
               alt={feature.title}
             />
-            <h5 className='mb-2 text-lg text-left font-semibold tracking-tight text-[#dff9fb]'>
+            <h5 className='mb-1 sm:mb-2 sm:text-lg text-[10px] text-left md:text-xl font-semibold tracking-tight text-[#000000]'>
               {feature.title}
             </h5>
-            <p className='mb-3 font-normal text-sm md:text-base text-[#ecf0f1] text-ellipsis leading-6 text-left'>
+            <p className='mb-1 sm:mb-3 font-normal text-[8px] leading-4 sm:text-sm md:text-base text-[#818181] text-ellipsis sm:leading-6 text-left'>
               {feature.description}
             </p>
           </div>
