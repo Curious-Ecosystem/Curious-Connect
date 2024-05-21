916382f6a5488b1d4c2bf25ffe17e22f381a1cab
anuragnegi000 <anurag.negi862@gmail.com>
2024-05-22 00:14:16 +0530

    [[Fixes]] #188 Redesigning of cards

--- frontend/src/Components/Pages/Landing-Page/MeetingCards.jsx
+++ frontend/src/Components/Pages/Landing-Page/MeetingCards.jsx
@@ -8,9 +8,8 @@ import { Link } from 'react-router-dom';
 
 const MeetingCards = () => {
   return (
-    <div className='flex  flex-wrap gap-0 justify-center w-full  text-cyan-50 text-2xl text-center'>
-      <IconContext.Provider
-        value={{ size: '3em', className: 'global-class-name' }}>
+    <div className='flex flex-wrap gap-0 justify-center w-full text-cyan-50 text-2xl text-center'>
+      <IconContext.Provider value={{ size: '1.5em', className: 'global-class-name' }}>
         <MeetingCard
           style={{ backgroundColor: '#ff742e' }}
           icon={<FaPlus />}
@@ -48,31 +47,17 @@ const MeetingCards = () => {
 export default MeetingCards;
 
 const MeetingCard = ({ style, icon, cardHeading, cardDesc }) => {
-  const [hovered, setHovered] = useState(false);
   return (
-    <div>
-      <div
-        style={style}
-        className={`flex flex-col justify-center  items-center  w-48  m-3  rounded-2xl h-48 transition duration-500 ease-in-out transform hover:scale-90 cursor-pointer ${
-          hovered ? 'shadow-md' : style.backgroundColor
-        }`}
-        onMouseEnter={() => setHovered(true)}
-        onMouseLeave={() => setHovered(false)}>
-        <p
-          className={`text-center text-xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-opacity duration-200 ease-in-out ${
-            hovered ? 'opacity-0' : 'opacity-100'
-          } `}>
-          {icon}
-        </p>
-        <div
-          className={`text-center  transition-opacity ease-in-out duration-300 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ${
-            hovered ? 'opacity-100' : 'opacity-0'
-          }`}>
-          <p className=' font-black text-xl mb-3'>{cardHeading}</p>
-          <p className=' w-full font-medium text-xs text-gray-200'>
-            {cardDesc}
-          </p>
-        </div>
+    <div
+      style={style}
+      className="relative flex flex-col justify-center items-center w-48 m-3 rounded-2xl h-48 transition duration-500 ease-in-out transform hover:scale-90 cursor-pointer shadow-md"
+    >
+      <div className="absolute top-2 left-2">
+        {icon}
+      </div>
+      <div className="text-center">
+        <p className="font-black text-xl mb-3">{cardHeading}</p>
+        <p className="w-full font-medium text-xs text-gray-200">{cardDesc}</p>
       </div>
     </div>
   );
