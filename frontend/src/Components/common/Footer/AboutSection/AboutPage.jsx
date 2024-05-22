57b0b1b838f7ab0c67edb636c695f9dc2f436e25
Ayush Kansal <ayushkansal303@gmail.com>
2024-05-23 00:33:52 +0530

    made the key features carousel from static to dynamic

--- frontend/src/Components/common/Footer/AboutSection/AboutPage.jsx
+++ frontend/src/Components/common/Footer/AboutSection/AboutPage.jsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import videoConf from '../../../../assets/About-Page-Assets/videoConf.png';
 import screenShare from '../../../../assets/About-Page-Assets/screenShared.svg';
@@ -27,15 +30,90 @@ const curiousFeatures = [
     'name': 'Customizable meeting settings',
     src: customMeet,
   },
-]
+];
+
+const sliderStyle = {
+  cursor: 'grab',
+};
 
 function AboutPage() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 600,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true, // Disable pausing on hover
+    cssEase: 'linear', // Add linear CSS easing
+    beforeChange: (oldIndex, newIndex) => {
+      setCurrentIndex(newIndex);
+    },
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1,
+        },
+      },
+    ],
+  };
+
+
+  const getCenterIndex = () => {
+    const totalSlides = curiousFeatures.length;
+    const slidesToShow = settings.slidesToShow;
+    const centerIndex = Math.floor(slidesToShow / 2);
+    return (currentIndex + centerIndex) % totalSlides;
+  };
+
+  const centerIndex = getCenterIndex();
+
   return (
     <div>
-      <div className='bg-gradient min-h-screen'>
-        <div className='max-w-4xl mx-auto py-12 px-4 sm:px-6 lg:px-8'>
+      <style>
+        {`
+            .center-slide {
+              transition: transform 0.5s ease, box-shadow 0.5s ease;
+              box-shadow: 0px 4px 20px rgba(0, 0, 0, .45);
+              
+              transform : scale(1.1);
+            }
+            .slick-slide {
+              display: flex;              
+              justify-content: center;
+              align-items: center;
+              height: 100%;
+            }
+            
+            .slick-track {              
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              height: 100%;
+              padding-top:3rem;
+              padding-bottom:3rem;
+            }
+          }
+  `}
+      </style>
+      <div className='bg-gradient min-h-screen flex items-center justify-center'>
+        <div className='max-w-5xl mx-auto py-12 px-4 sm:px-6 lg:px-8'>
           <h1 className='text-3xl font-extrabold text-gray-100 mb-4'>
-            About Curious Connect
+            About Us
           </h1>
           <p className='text-lg text-gray-100 mb-6'>
             Curious Connect is your go-to platform for revolutionizing virtual
@@ -53,26 +131,32 @@ function AboutPage() {
             Explore Curious Connect to redefine how you collaborate online and
             unlock new levels of productivity and engagement.
           </p>
-          <div className='bg-white rounded-lg shadow-md p-6'>
+          <div className='bg-white rounded-lg shadow-md p-6 max-h-full'>
             <h2 className='text-2xl font-semibold text-gray-900 mb-4'>
               Key Features
             </h2>
-            <ul className='list-none list-inside text-lg flex flex-wrap justify-center text-center text-gray-700'>
-              {curiousFeatures.map((i, key) => {
-                return (
-                  <li key={key} className='w-48 mb-6 flex justify-center items-center flex-col'>
-                    <img className='h-28 mb-2' src={i.src} alt={i.name} />
-                    <span>
-                      {i.name}
-                    </span>
-                  </li>
-                )
-              })}
-            </ul>
+            <Slider {...settings} >
+              {curiousFeatures.map((feature, index) => (
+                <div
+                  key={index}
+                  style={sliderStyle}
+                  onMouseDown={(e) => (e.currentTarget.style.cursor = 'grabbing')}
+                  onMouseUp={(e) => (e.currentTarget.style.cursor = 'grab')}
+                  onMouseLeave={(e) => (e.currentTarget.style.cursor = 'grab')}
+                  className={`flex justify-center items-center flex-col mx-auto bg-white p-4 rounded-lg ${index === centerIndex ? 'center-slide' : ''
+                    }`}
+                >
+                  <div className="flex text-center items-center flex-col">
+                    <img className='h-28 mb-2' src={feature.src} alt={feature.name} />
+                    <span>{feature.name}</span>
+                  </div>
+                </div>
+              ))}
+            </Slider>
           </div>
         </div>
       </div>
-    </div >
+    </div>
   );
 }
 
