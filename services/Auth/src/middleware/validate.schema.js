46037f017cb39bc5b0a4973cd23cc54a7b7ad4cc
CDCNaman <naman@curiousdevelopers.in>
2024-06-06 15:09:20 +0530

    FIX: Error Handling Middleware Added

--- services/Auth/src/middleware/validate.schema.js
+++ services/Auth/src/middleware/validate.schema.js
@@ -1,14 +1,13 @@
+const { errorHandler } = require('../utils/error');
+
 const validate = (schema) => async (req, res, next) => {
     try {
         const parsedBody = await schema.parseAsync(req.body);
         req.body = parsedBody; 
         next();
     } catch (err) {
-        console.log(err);
         const yourerror = err.errors[0].message;
-        res.status(400).json({
-            msg: yourerror,
-        });
+        next(errorHandler(400, yourerror, err));
     }
 }
 
