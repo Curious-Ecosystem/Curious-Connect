36b13b336a1a6f444b645f14c13e686678978465
Harsh Tripathi <alpharsh1@gmail.com>
2024-07-31 15:12:31 +0530

    Improved logo font in the Navbar

--- frontend/src/Components/Pages/Landing-Page/Navbar.jsx
+++ frontend/src/Components/Pages/Landing-Page/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
             whileHover={{ scale: 1.1, rotate: [0, -10, 0, 10, 0], transition: { duration: 0.3 } }}
           />
         </Link>
-        <span className='text-center font-bold'>
+        <span className='font-bold text-xl md:text-3xl'>
           <span className='text-blue-400'>Curious</span> Connect
         </span>
       </div>
