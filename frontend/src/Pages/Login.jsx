61afb0999a0455729b7a271793d2437a42e4af66
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2024-08-10 13:32:09 +0530

    Fixing the Bug and Do Styling Also Remove Unwanted Material

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,107 +1,222 @@
-import { Box, Button, Heading, Input } from '@chakra-ui/react'
-import React, { useState } from 'react'
+import { Box, Button, Heading, Input, VStack, Divider, HStack } from '@chakra-ui/react';
+import React, { useState } from 'react';
 import { signInWithPopup, createUserWithEmailAndPassword, signOut, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth, db, provider } from "../Firebase/firebase-config";
 import { doc, setDoc } from 'firebase/firestore';
 
-
 const Login = () => {
+  const [emailSignUp, setEmailSignUp] = useState('');
+  const [passwordSignUp, setPasswordSignUp] = useState('');
 
-     const [emailSignUp, setEmailSignUp] = useState('')
-     const [passwordSignUp, setPasswordSignUp] = useState('')
+  const [emailSignIn, setEmailSignIn] = useState('');
+  const [passwordSignIn, setPasswordSignIn] = useState('');
 
-     const [emailSignIn, setEmailSignIn] = useState('')
-     const [passwordSignIn, setPasswordSignIn] = useState('')
-     const Signup = async () => {
-          try {
-               const email = emailSignUp;
-               const password = passwordSignUp;
+  const [showSignUp, setShowSignUp] = useState(true); // State to toggle between Sign Up and Sign In forms
 
-               const userCredential = await createUserWithEmailAndPassword(auth, email, password)
-               const user = userCredential.user;
+  const Signup = async () => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, emailSignUp, passwordSignUp);
+      const user = userCredential.user;
+      const usersCollectionRef = doc(db, 'users', user.uid);
+      await setDoc(usersCollectionRef, { email: emailSignUp });
+      setEmailSignUp('');
+      setPasswordSignUp('');
+    } catch (error) {
+      console.error('Signup error: ', error);
+    }
+  };
 
-               const usersCollectionRef = doc(db, 'users', user.uid);
-               await setDoc(usersCollectionRef, { email, password })
+  const SignIn = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, emailSignIn, passwordSignIn);
+      setEmailSignIn('');
+      setPasswordSignIn('');
+    } catch (error) {
+      console.error('SignIn error: ', error);
+    }
+  };
 
-               setEmailSignUp("");
-               setPasswordSignUp("");
-          } catch (error) {
-               console.log('error: ', error);
-          }
-     }
+  const signInWithGoogle = async () => {
+    try {
+      const userCredential = await signInWithPopup(auth, provider);
+      const user = userCredential.user;
+      const usersCollectionRef = doc(db, 'users', user.uid);
+      await setDoc(usersCollectionRef, { email: user.email, googleAuth: true });
+    } catch (error) {
+      console.error('Google SignIn error: ', error);
+    }
+  };
 
-     const SignIn = async () => {
-          try {
-               const email = emailSignIn;
-               const password = passwordSignIn;
+  const logout = async () => {
+    try {
+      await signOut(auth);
+      alert('Logged out');
+    } catch (error) {
+      console.error('Logout error: ', error);
+    }
+  };
 
-               const userCredential = await signInWithEmailAndPassword(auth, email, password);
-               const user = userCredential.user;
+  return (
+    <Box
+      minH="100vh"
+      display="flex"
+      alignItems="center"
+      justifyContent="center" 
+      p={4}
+    >
+      <Box
+        p={15}
+        maxW="lg"
+        borderWidth={4}
+        borderRadius="18px"
+        boxShadow="lg"
+        bg="rgba(255, 255, 255, 0.1)"
+        backdropFilter="blur(18px)" // Frosted glass effect
+        borderColor="rgba(255, 255, 255, 0.2)"
+        _hover={{ boxShadow: 'xl', transform: 'scale(1.02)' }}
+        transition="all 0.3s ease"
+      >
+        <HStack spacing={100} mb={20} justifyContent="center">
+          <Button
+            colorScheme="teal"
+            color={'white'}
+            variant={showSignUp ? 'solid' : 'outline'}
+            onClick={() => setShowSignUp(true)}
+          >
+            Sign Up
+          </Button>
+          <Button
+            colorScheme="teal"
+            color={'white'}
+            variant={!showSignUp ? 'solid' : 'outline'}
+            onClick={() => setShowSignUp(false)}
+          >
+            Sign In
+          </Button>
+        </HStack>
 
-               setEmailSignIn("")
-               setPasswordSignIn("")
-          } catch (error) {
-               console.log('error: ', error);
-          }
-     }
+        {showSignUp ? (
+          <VStack spacing={7}>
+            <Heading as="h3" size="lg" color="white" fontFamily="'Poppins', sans-serif" fontWeight="semibold">Sign Up</Heading>
+            <Input
+              variant="outline"
+              placeholder="Email"
+              type="Email"
+              value={emailSignUp}
+              onChange={(e) => setEmailSignUp(e.target.value)}
+              mb={3}
+              borderColor="teal.300"
+              borderRadius="10px"
+              _focus={{ borderColor: 'teal.400', boxShadow: '0 0 0 1px teal.400' }}
+              transition="all 0.2s ease"
+              fontFamily="'Poppins', sans-serif"
+              bg="rgba(255, 255, 255, 0.1)" // Semi-transparent background
+              color="white"
+            />
+            <Input
+              variant="outline"
+              placeholder="Password"
+              type="password"
+              value={passwordSignUp}
+              onChange={(e) => setPasswordSignUp(e.target.value)}
+              mb={5}
+              borderColor="teal.300"
+              borderRadius="10px"
+              _focus={{ borderColor: 'teal.400', boxShadow: '0 0 0 1px teal.400' }}
+              transition="all 0.2s ease"
+              fontFamily="'Poppins', sans-serif"
+              bg="rgba(255, 255, 255, 0.1)" // Semi-transparent background
+              color="white"
+            />
+            <Button
+              colorScheme="teal"
+              width="full"
+              _hover={{ bg: 'teal.400', color: 'white' }}
+              _active={{ bg: 'teal.500', color: 'white' }}
+              borderRadius="md"
+              transition="all 0.2s ease"
+              fontFamily="'Poppins', sans-serif"
+              color="white"
+            >
+              Sign Up
+            </Button>
+            <Button
+              variant="outline"
+              colorScheme="teal"
+              width="full"
+              borderRadius="md"
+              _hover={{ borderColor: 'teal.500', color: 'teal.500' }}
+              transition="all 0.2s ease"
+              fontFamily="'Poppins', sans-serif"
+              onClick={signInWithGoogle}
+              color="white"
+            >
+              Sign Up with Google
+            </Button>
+          </VStack>
+        ) : (
+          <VStack spacing={6}>
+            <Heading as="h3" size="md" color="white" fontFamily="'Poppins', sans-serif" fontWeight="semibold">Sign In</Heading>
+            <Input
+              variant="outline"
+              placeholder="Email"
+              type="Email"
+              value={emailSignIn}
+              onChange={(e) => setEmailSignIn(e.target.value)}
+              mb={3}
+              borderColor="teal.300"
+              borderRadius="12px"
+              _focus={{ borderColor: 'teal.400', boxShadow: '0 0 0 1px teal.400' }}
+              transition="all 0.2s ease"
+              fontFamily="'Poppins', sans-serif"
+              bg="rgba(255, 255, 255, 0.2)" // Semi-transparent background
+              color="white"
+            />
+            <Input
+              variant="outline"
+              placeholder="Password"
+              type="password"
+              value={passwordSignIn}
+              onChange={(e) => setPasswordSignIn(e.target.value)}
+              mb={4}
+              borderColor="teal.300"
+              borderRadius="12px"
+              _focus={{ borderColor: 'teal.400', boxShadow: '0 0 0 1px teal.400' }}
+              transition="all 0.2s ease"
+              fontFamily="'Poppins', sans-serif"
+              bg="rgba(255, 255, 255, 0.2)" // Semi-transparent background
+              color="white"
+            />
+            <Button
+              colorScheme="teal"
+              width="full"
+              _hover={{ bg: 'teal.400', color: 'white' }}
+              _active={{ bg: 'teal.500', color: 'white' }}
+              borderRadius="md"
+              transition="all 0.2s ease"
+              fontFamily="'Poppins', sans-serif"
+              color="white"
+            >
+              Sign In
+            </Button>
+            <Button
+              variant="outline"
+              colorScheme="teal"
+              width="full"
+              borderRadius="md"
+              _hover={{ borderColor: 'teal.500', color: 'teal.500' }}
+              transition="all 0.2s ease"
+              fontFamily="'Poppins', sans-serif"
+              onClick={signInWithGoogle}
+              color="white"
+            >
+              Sign In with Google
+            </Button>
+          </VStack>
+        )}
+      </Box>
+    </Box>
+  );
+};
 
-     const signInWithGoogle = async () => {
-          try {
-               const userCredential = await signInWithPopup(auth, provider)
-               const user = userCredential.user
-               const name = user.displayName;
-               const email = user.email;
-               const profilePic = user.photoURL;
-
-               const usersCollectionRef = doc(db, 'users', user.uid);
-               await setDoc(usersCollectionRef, { email, googleAuth: true });
-
-          } catch (error) {
-               console.log('error: ', error);
-          }
-     }
-
-     // * Logout
-     const logout = async () => {
-          try {
-               await signOut(auth);
-               alert("logout")
-          } catch (error) {
-               console.log('error: ', error);
-          }
-     }
-
-     return (
-          <Box  className='bg-blue-200' display={'flex'}>
-          
-               <Heading textAlign={'center'}>Sign Up</Heading>
-               <Box w='30%' m='auto'>
-                    <Box>
-                         <Input type='email' placeholder='Email' value={emailSignUp} my='2' onChange={e => setEmailSignUp(e.target.value)} />
-                    </Box>
-                    <Box>
-                         <Input type='password' placeholder='Password' my='2' value={passwordSignUp} onChange={e => setPasswordSignUp(e.target.value)} />
-                    </Box>
-
-                    <Button  className='bg-white' onClick={Signup}>Sign Up</Button>
-                    <Button my='2' onClick={signInWithGoogle}>Sign In Google</Button>
-               </Box>
-
-               <Heading textAlign={'center'}>Sign In</Heading>
-               <Box w='30%' m='auto'>
-                    <Box>
-                         <Input type='email' placeholder='Email' value={emailSignIn} my='2' onChange={e => setEmailSignIn(e.target.value)} />
-                    </Box>
-                    <Box>
-                         <Input type='password' placeholder='Password' my='2' value={passwordSignIn} onChange={e => setPasswordSignIn(e.target.value)} />
-                    </Box>
-
-                    <Button onClick={SignIn}>Sign In</Button>
-                    <Button my='2' className='bg-white' onClick={signInWithGoogle}>Sign In Google</Button>
-                    <Button m='2' onClick={logout}>Logout</Button>
-               </Box>
-          </Box>
-     )
-}
-
-export default Login
+export default Login;
